b92945d9dc1c89e1f27c0a04b04c795e53bde9ce
Enoch Essel <enochessel5@gmail.com>
2026-03-24 21:51:58 +0000

    Fix Express 5 wildcard route syntax error

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use('/api/logs', logRoutes);
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
